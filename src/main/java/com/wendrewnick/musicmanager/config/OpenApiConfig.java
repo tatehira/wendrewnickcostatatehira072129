5dd886544bbbe059323651b825449aac57eeb77d
tatehira <niickcostta00@outlook.com>
2026-01-22 20:01:59 -0400

    Ajustes: Documentação Swagger

--- src/main/java/com/wendrewnick/musicmanager/config/OpenApiConfig.java
+++ src/main/java/com/wendrewnick/musicmanager/config/OpenApiConfig.java
@@ -9,16 +9,15 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Music Manager API", version = "1.0", description = "API Enterprise para gerenciamento de Álbuns, Artistas e Integrações Regionais.", contact = @Contact(name = "Equipe de Desenvolvimento", email = "dev@musicmanager.com"), license = @License(name = "Apache 2.0", url = "http://springdoc.org")), servers = {
+@OpenAPIDefinition(info = @Info(title = "Music Manager API", version = "1.0", description = "API para gerenciamento de Álbuns e Artistas.", contact = @Contact(name = "desenvolvedor", email = "nickdebian@outlook.com")), servers = {
                 @Server(url = "/", description = "Default Server URL") }, tags = {
                                 @Tag(name = "Autenticação", description = "Endpoints para Login e atualização de Token"),
                                 @Tag(name = "Artistas", description = "Endpoints para gerenciamento de Artistas"),
                                 @Tag(name = "Álbuns", description = "Endpoints para gerenciamento de Álbuns")
-                }, externalDocs = @ExternalDocumentation(description = "Documentação Completa", url = "https://github.com/wendrewnick/music-manager-api"))
+                }, externalDocs = @ExternalDocumentation(description = "Documentação Completa", url = "https://github.com/tatehira/wendrewnickcostatatehira072129.git"))
 @SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
 public class OpenApiConfig {
 }
