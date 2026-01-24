60da29dbd4620927763fafb0f84301d96eca63c2
tatehira <niickcostta00@outlook.com>
2026-01-24 02:54:53 -0400

    Corrigido confitos

--- src/main/java/com/wendrewnick/musicmanager/config/WebSocketConfig.java
+++ src/main/java/com/wendrewnick/musicmanager/config/WebSocketConfig.java
@@ -26,7 +26,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:8080", "http://localhost:3000")
+                .setAllowedOrigins(ALLOWED_ORIGINS.toArray(String[]::new))
                 .withSockJS();
     }
 }
